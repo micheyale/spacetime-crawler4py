278ca15b8a0ec3c2613984446e1c1eb6d5c0e04f
sudney <46584496+sudney@users.noreply.github.com>
2020-11-03 13:18:12 -0800

    Update scraper.py
    michelle and sydney update. consolidated the code a bit

--- scraper.py
+++ scraper.py
@@ -11,12 +11,12 @@ from datetime import datetime
 path_dict = {}
 ics_subdomain_dict = dict()
 common_word = {}
-tokenDict = {}
+
 longest_page = ("",0)
 
-average = []
-
-
+#THIS ERROR NEEDS TO BE ACCOUNTED FOR
+#https://password.ics.uci.edu
+#error: urllib.error.HTTPError: HTTP Error 308: Permanent Redirect
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -63,8 +63,8 @@ def validate2(url):
     
 def extract_next_links(url, resp):
     lst = []
+    tokenDict = {}
     global longest_page
-    global tokenDict
     if resp.status == 200:
         html = urllib.request.urlopen(url).read()
         bodyText = text_from_html(html)
@@ -76,35 +76,35 @@ def extract_next_links(url, resp):
         parsed_uri = urlparse(url)
         result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
 
-        word_count = sum(tokenDict[d] for d in tokenDict if d.isalpha())
+        word_count = sum(tokenDict[d] for d in tokenDict if d.isalpha()) #only counting words on a page rn 
         print("WORD COUNT: ",word_count)
         if word_count > longest_page[1]:
             longest_page = (url,word_count)
         average.append(word_count)
-        tokenDict = {}
-        for link in links_lst:
         
+        for link in links_lst:
+            
             if 'href' in link.attrs:
-                missing_domain_check = result + link.attrs['href']
-                         
-                in_domain = re.search('https?://([a-z0-9]+[.])*uci[.]edu((\/\w+)*\/)?',link.attrs['href'])
-                also_in_domain = re.search('https?://([a-z0-9]+[.])*uci[.]edu((\/\w+)*\/)?',missing_domain_check)
                 current_link = link.attrs['href']
+                if len(link.attrs['href']) >= 1 and link.attrs['href'][0] == '/': #adding the parent domain to keys that only equal the path
+                    print("ONLY PATH: ",link.attrs['href'])
+                    missing_domain_check = result + link.attrs['href']
+                    current_link = missing_domain_check
                 
-                if also_in_domain and not in_domain:
-                    if len(link.attrs['href']) >= 1 and link.attrs['href'][0] == '/':
-                        current_link = missing_domain_check
-                    else:
-                        also_in_domain = False
+                in_domain = re.search('https?://([a-z0-9]+[.])*uci[.]edu((\/\w+)*\/)?',current_link)
+                also_in_domain = re.search('https?://([a-z0-9]+[.])*uci[.]edu((\/\w+)*\/)?',current_link)
+                                
+
                 if '#' in current_link:
                     current_link = current_link.split('#')[0]
                     print("LINK HAS BEEN DEFRAGGED: ",current_link)
-                if '?' in current_link:
-                    current_link = current_link.split('?')[0] #this needs to be changed
-                    print("This link no longer has parameters ",current_link)       
+                #I INITIALLY HAD THIS TO AVOID A TRAP BUT I DON'T THINK WE'RE GETTING STUCK IN IT NOW
+                #if '?' in current_link:
+                #    current_link = current_link.split('?')[0] #this needs to be changed
+                #    print("This link no longer has parameters ",current_link)
             
-                print(current_link)
-                if  (in_domain or also_in_domain) and is_valid(current_link) and current_link not in path_dict:
+                #PATH_DICT NEEDS TO UNIFORMLY BE ADDING HTTP:// TO ALL LINK SO WE DON'T GET TWO VERSION OF THE SAME LINK           tentative value
+                if (in_domain or also_in_domain) and is_valid(current_link) and current_link not in path_dict and if word_count > 150:
     
                     print(current_link)
                     lst.append(current_link)
@@ -120,10 +120,8 @@ def extract_next_links(url, resp):
                             ics_subdomain_dict[no_path] += 1                                 
             
 
-
     
     print("ICS DICT: ", ics_subdomain_dict)                
-    #print(sum(average)/len(average))
     print("LONGEST PAGE: ",longest_page)
     return lst
 
@@ -135,17 +133,17 @@ def is_valid(url):
         content_type = check.headers.get('content-type')
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if 'calendar' in url: #here for now just to get rid of obvious cases but want to change
-            return False
+        #if 'calendar' in url: #here for now just to get rid of obvious cases but want to change
+        #    return False
         if validate(url):  
             return False
         if validate2(url):  
             return False
         if check.status_code != 200:
             return False
-        if 'pdf' in content_type: #make then apply to more types that are getting in there
+        if content_type and 'pdf' in content_type: #make then apply to more types that are getting in there
             return False
-        #if \event\ is in there don't search
+        
 
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -157,9 +155,9 @@ def is_valid(url):
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
     except requests.ConnectionError:
-        print(current_link, " is not a valid website.")
+        print(url, " is not a valid website.")
     except requests.exceptions.InvalidURL:
-        print(current_link, " is not a valid website: no host specified.")
+        print(url, " is not a valid website: no host specified.")
 
     except TypeError:
         print ("TypeError for ", parsed)
