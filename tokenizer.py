5f58821e89423ffc6911327b0554b61774e7102e
Mahsa Clinton <62405418+mahsa7haft@users.noreply.github.com>
2020-11-04 20:52:07 -0800

    Add files via upload

--- tokenizer.py
+++ tokenizer.py
@@ -2,36 +2,35 @@
 
 import sys
 import re
-from nltk.corpus import stopwords
-#from nltk.tokenize import word_tokenize
 
+tokenMatch = re.compile("[\s.,!?:;'\"-]+")
 # runtime: linear O(n) where n is number of tokens in a text file
-# description:	each line of a file is grabbed then iterated over,
-#				if token is found it is added to list
+# description:    each line of a file is grabbed then iterated over,
+#                if token is found it is added to list
 def getTokens(string: str) -> list:
     tokens = []
-    cleanLine = re.split("\W+", string)
+    cleanLine = tokenMatch.split(string)
+
     for word in cleanLine:
-        if word.isalnum() == True:
+        if word.replace("'", "").isalnum() == True:
             tokens.append(word.lower())
-        return tokens
+
+    return tokens
 
 # runtime: linear O(n) where n is size of tokens list
-# description:	tokens list is iterated over, add tokens as keys to
-#				dict counting # of occurences of each token
+# description:    tokens list is iterated over, add tokens as keys to
+#                dict counting # of occurences of each token
 def computeWordFrequencies(tokenDict: dict, tokens: list) -> dict:
     for t in tokens:
-            
         if t not in tokenDict:
             tokenDict[t] = 0
-	
         tokenDict[t] += 1
-    updateStopWords(tokenDict)
+
     return tokenDict
 
 # runtime: log-linear O(n*log(n)) where n is number of dict keys
-# description:	sort freqencies dict in descending order, then print
-#				all key -> value pairs
+# description:    sort freqencies dict in descending order, then print
+#                all key -> value pairs
 def printFreqs(freqs: dict) -> None:
     sortedFreqs = sorted(freqs.items(), key=lambda x: x[1], reverse=True)
 
@@ -39,22 +38,9 @@ def printFreqs(freqs: dict) -> None:
         print(f[0] + " = " + str(f[1]))
 
 # runtime: log-linear O(n*log(n)) where n is number of real tokens
-# description:	entry point. find tokens, build frequency dict, then
-#				sort and print frequency list. bound by printFreqs()
+# description:    entry point. find tokens, build frequency dict, then
+#                sort and print frequency list. bound by printFreqs()
 def updateTokenCounts(tokenDict: dict, bodyText: str):
     tokens = getTokens(bodyText)
     freqs = computeWordFrequencies(tokenDict, tokens)
-
-
-def updateStopWords(tokenDict: dict):
-    stop_words = set(stopwords.words('english'))
-    #print(stop_words)
-    for i in stop_words:
-        try:
-            del tokenDict[i]
-        except (KeyError):
-            continue
-    return tokenDict
-    
-
 
